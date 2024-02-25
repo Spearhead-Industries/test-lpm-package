968c25ba8d44e6ba691f566ef9b6d2cc9b839663
Plain English <20248750+plainenglishh@users.noreply.github.com>
2024-02-25 15:37:29 +0000

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,9 @@
+local lpm_dep_2 = require("@lpm/lpm-dep-2");
+
 local test_package = {};
 
 function test_package.say_hi()
-  print("Hi!");
+  print(lpm_dep_2.get_hi());
 end
 
 return test_package;
